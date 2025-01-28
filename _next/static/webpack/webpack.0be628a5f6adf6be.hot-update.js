a76202e1e928bc64361c0434c7a96d411a1dafd8
ravitejamanchala <manchala0024@gmail.com>
2025-01-28 03:59:08 +0000

    Updates

--- _next/static/webpack/webpack.0be628a5f6adf6be.hot-update.js
+++ _next/static/webpack/webpack.0be628a5f6adf6be.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ (() => {
-/******/ 	__webpack_require__.h = () => ("d39f58048688b6c8")
+/******/ 	__webpack_require__.h = () => ("ecc0181ec37f621b")
 /******/ })();
 /******/ 
 /******/ }
