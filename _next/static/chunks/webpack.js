e46396041c0ea44df713eebddc0ff1875604ae12
ravitejamanchala <manchala0024@gmail.com>
2025-01-28 11:57:15 +0000

    Updates

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -161,7 +161,7 @@
 /******/ 		// This function allow to reference async chunks
 /******/ 		__webpack_require__.u = (chunkId) => {
 /******/ 			// return url for filenames based on template
-/******/ 			return undefined;
+/******/ 			return "static/chunks/" + chunkId + ".js";
 /******/ 		};
 /******/ 	})();
 /******/ 	
@@ -190,7 +190,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("16d98061a00b17e1")
+/******/ 		__webpack_require__.h = () => ("93ede0ba1a70e2ce")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
